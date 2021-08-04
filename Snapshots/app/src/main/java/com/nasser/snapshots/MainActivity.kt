d8b5747e096fa7fdc6f2760ec5d1a61223ceb243
jnasser513 <00245818@uca.edu.sv>
2021-08-03 23:51:00 -0600

    Adding fragment change in bottom navigation buttons

--- Snapshots/app/src/main/java/com/nasser/snapshots/MainActivity.kt
+++ Snapshots/app/src/main/java/com/nasser/snapshots/MainActivity.kt
@@ -2,10 +2,68 @@ package com.nasser.snapshots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.nasser.snapshots.databinding.ActivityMainBinding
+import com.nasser.snapshots.fragment.AddFragment
+import com.nasser.snapshots.fragment.HomeFragment
+import com.nasser.snapshots.fragment.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.host_fragment , profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment).commit()
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.host_fragment , addFragment, AddFragment::class.java.name)
+            .hide(addFragment).commit()
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.host_fragment , homeFragment, HomeFragment::class.java.name)
+            .commit()
+
+        mBinding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(homeFragment).commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+                R.id.action_home_add -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(addFragment).commit()
+                    mActiveFragment = addFragment
+                    true
+                }
+                R.id.action_home_profile -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(profileFragment).commit()
+                    mActiveFragment = profileFragment
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
